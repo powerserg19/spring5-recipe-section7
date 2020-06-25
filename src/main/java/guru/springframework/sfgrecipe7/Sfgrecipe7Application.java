bff857aca731b467f6a543c4dddd993ecbd54306
sergio <powerserg19@hotmail.com>
2020-06-25 04:59:21 -0400

    end of section 98

---
+++ src/main/java/guru/springframework/sfgrecipe7/Sfgrecipe7Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipe7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sfgrecipe7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sfgrecipe7Application.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgrecipe7/Sfgrecipe7ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipe7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sfgrecipe7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
